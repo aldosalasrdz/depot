05f7fd78bfa09ab2fc29bd55d32d91fe9085c2c9
Aldo Salas <aldosalasrdz@gmail.com>
2024-12-25 19:01:53 -0600

    Change title uniqueness validation error message

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: {
+    message: "already in use. Try another one."
+  }
   validates :title, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
