2115072eaace0c1656a22ff15240d8b15e7f64ef
Aldo Salas <aldosalasrdz@gmail.com>
2024-12-28 22:58:46 -0600

    Cart Creation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: {
     message: "already in use, try another one."
@@ -9,4 +13,14 @@ class Product < ApplicationRecord
     message: "must be a URL for GIF, JPG or PNG image"
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+
+    # Ensure that there are not line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line Items present")
+        throw :abort
+      end
+    end
 end
