aad69f0ef1e8d891915552c3b3c792603d24dc47
Aldo Salas <aldosalasrdz@gmail.com>
2025-01-12 00:42:25 -0600

    Fix migration typo

--- db/migrate/20250112055338_add_payment_type_info_to_orders.rb
+++ db/migrate/20250112055338_add_payment_type_info_to_orders.rb
@@ -2,7 +2,7 @@ class AddPaymentTypeInfoToOrders < ActiveRecord::Migration[7.2]
   def change
     add_column :orders, :routing_number, :string, null: true
     add_column :orders, :account_number, :string, null: true
-    add_column :orders, :credit_cart_number, :string, null: true
+    add_column :orders, :credit_card_number, :string, null: true
     add_column :orders, :expiration_date, :string, null: true
     add_column :orders, :po_number, :string, null: true
   end
