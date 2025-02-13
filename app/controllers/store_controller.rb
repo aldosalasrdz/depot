fd3c8f41b87d5adce104ddea67ced190d1002fa8
Aldo Salas <aldosalasrdz@gmail.com>
2025-02-12 19:54:02 -0600

    Iteration J4: Adding a Locale Switcher

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,7 +5,11 @@ class StoreController < ApplicationController
   skip_before_action :authorize
 
   def index
-    @products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+    end
 
     respond_to do |format|
       format.html
