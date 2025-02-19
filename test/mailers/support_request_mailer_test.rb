f76b14b2919ca95a5b8c362188807cb87b17a4bc
Aldo Salas <aldosalasrdz@gmail.com>
2025-02-18 20:09:36 -0600

    Fix mailer tests

--- test/mailers/support_request_mailer_test.rb
+++ test/mailers/support_request_mailer_test.rb
@@ -2,10 +2,9 @@ require "test_helper"
 
 class SupportRequestMailerTest < ActionMailer::TestCase
   test "respond" do
-    mail = SupportRequestMailer.respond
-    assert_equal "Respond", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
+    mail = SupportRequestMailer.respond(support_requests(:one))
+    assert_equal "Re: Wrong book delivered", mail.subject
+    assert_equal [ "dave@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
   end
 end
